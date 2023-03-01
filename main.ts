dfa715ac9a8cc713271fbc7a6d1aac5599cc5c84
antoniojesuslaracriado <123951175+antoniojesuslaracriado@users.noreply.github.com>
2023-03-01 14:47:01 +0100

    baso 1

--- main.ts
+++ main.ts
@@ -10,8 +10,17 @@ basic.forever(function () {
     }
     if (dis != 0 && dis < 20) {
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
-        if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) != 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) < 1) {
-            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
-        }
+    }
+    if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) != 0 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) != 0) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 40)
+        basic.pause(1000)
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 40)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 40)
+        basic.pause(500)
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    }
+    if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) != 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) != 1) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
     }
 })
